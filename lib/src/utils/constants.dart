bc2c839f8740380824492884fd66fb29f3c333e8
AnastasiaTomson <anastasia.tomson@mail.ru>
2024-05-19 10:50:06 +0700

    Первый коммит

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,6 +1,4 @@
 // Material
-import 'package:flutter/material.dart';
-
 const String materialAppTitle = 'FootMobile';
 
 // Service
@@ -11,18 +9,3 @@ class RouteNames {
   static String home = 'home';
   static String championship = 'championship';
 }
-
-// Colors
-const List<Color> colors = [
-  Color(0xFFFF2D2D),
-  Color(0xFFFF782D),
-  Color(0xFFFFC42D),
-  Color(0xFF07D71C),
-  Color(0xFF00E5A1),
-  Color(0xFF00BCE5),
-  Color(0xFF0077E5),
-  Color(0xFF3C00E5),
-  Color(0xFF9C00E5),
-  Color(0xFFEC3EDA),
-  Color(0xFFEC3E72),
-];
